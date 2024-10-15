0bd8fe798d007e38f598f805abd4a45de8474529
lee <lichengfxf@163.com>
2024-10-15 18:35:09 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,30 +2,9 @@
 
 set -e
 
-. config.sh
-
-[ -d output ] && rm -rf output || true
-[ -d review ] && rm -rf review || true
 mkdir -p output review
+docker build --tag shim_build . 2>&1 | tee review/build-log.txt
 
-set -e
-
-build_args=(buildx build --tag shim_build)
-build_args+=(--build-arg SHIM_ARCHIVE_URL=${SHIM_ARCHIVE_URL})
-build_args+=(--build-arg SHIM_ARCHIVE_FILE=${SHIM_ARCHIVE_FILE})
-build_args+=(--build-arg SHIM_ARCHIVE_SHA256=${SHIM_ARCHIVE_SHA256})
-build_args+=(--build-arg EFIDIR=${EFIDIR})
-build_args+=(--output type=local,dest=output)
-if [ "${BUILDX_USE_CACHE}" = "y" ]; then
-	build_args+=(--cache-from=type=local,src=/tmp/.buildx-cache)
-	build_args+=(--cache-to=type=local,dest=/tmp/.buildx-cache-new)
-fi
-build_args+=(.)
-
-docker "${build_args[@]}" 2>&1 | tee review/build-log.txt
-
-cp $(find output -type f -name "shim*.efi") review/
-cp output/*.txt review/
-
-( cd review && sha256sum shim*.efi > hashs.txt )
+# for name in $(find ./output/ -type f -name "shim*.efi"); do cp -av $name ./review; done 
+# ( cd review && sha256sum shim*.efi > hashs.txt )
 
--- config.sh
+++
@@ -1,6 +0,0 @@
-SHIM_ARCHIVE_URL=https://github.com/rhboot/shim/releases/download/15.8/shim-15.8.tar.bz2
-SHIM_ARCHIVE_FILE=shim-15.8.tar.bz2
-SHIM_ARCHIVE_SHA256=a79f0a9b89f3681ab384865b1a46ab3f79d88b11b4ca59aa040ab03fffae80a9
-
-EFIDIR=shenxinda
-
