f8d1d0ecc503ecca7bae90205c1b7048321e3fc2
lee <lichengfxf@163.com>
2024-10-29 09:54:01 +0800

    update cert and rebuild

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 mkdir -p review
+#docker rmi -f shim_build
 docker build --tag shim_build . 2>&1 | tee review/build-log.txt
 
 # for review
-sudo docker run --rm -v $(pwd)/review:/mnt shim_build bash -c "find /work/output/ -type f -name 'shim*.efi' -exec cp -av {} /mnt \; && sha256sum /mnt/shim*.efi > /mnt/hashs.txt"
+# sudo docker run --rm -v $(pwd)/review:/mnt shim_build bash -c "find /work/output/ -type f -name 'shim*.efi' -exec cp -av {} /mnt \; && sha256sum /mnt/shim*.efi > /mnt/hashs.txt"
